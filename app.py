7545749f6352661691dbb729a8a4a10a4c74276e
DongHwan-Park <pdh99045@naver.com>
2022-12-26 12:43:34 +0900

    Create python streamlit project

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from time import sleep
+
+
+# 페이지 기본 설정
+st.set_page_config(
+    page_icon ="😂",
+    page_title="빅공뱀의 스트림릿",
+    layout='wide',
+)
